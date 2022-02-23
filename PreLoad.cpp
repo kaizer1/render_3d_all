19058f060cd9c3350c830fa371ebb7e3e18ce106
Sergei Loskutnikov <nitche9@gmail.com>
2022-02-23 12:43:58 +0300

    directx main start

--- PreLoad.cpp
+++ PreLoad.cpp
@@ -37,7 +37,7 @@ PreLoad::PreLoad() {
 
  
 
-      std::cout <<  " count my building ! " << "\n";
+      std::cout <<  " count my Direct X  ! " << "\n";
      
         if (waitOrNorToExitConsole){
            
